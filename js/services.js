9ea57420d2a8ff9cea884c11b3e5a243dfbf7fe3
ga12ru12 <quang123>
2014-11-07 10:50:33 +0700

    123

--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
 /**
  * Created by Sandeep on 29/08/14.
  */
-angular.module('com.htmlxprs.imageShare.services',[]).value('USER',{}).value('SOCKET_URL','http://shareimage-ga12ru12.rhcloud.com/');
+angular.module('com.htmlxprs.imageShare.services',[]).value('USER',{}).value('SOCKET_URL','shareimage-ga12ru12.rhcloud.com');
