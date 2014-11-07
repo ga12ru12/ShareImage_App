60c817aef273e329c7ec36c7c1f29155d9058477
ga12ru12 <quang123>
2014-11-07 11:09:03 +0700

    config socket openshit 8443

--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
 /**
  * Created by Sandeep on 29/08/14.
  */
-angular.module('com.htmlxprs.imageShare.services',[]).value('USER',{}).value('SOCKET_URL','shareimage-ga12ru12.rhcloud.com');
+angular.module('com.htmlxprs.imageShare.services',[]).value('USER',{}).value('SOCKET_URL','ws://shareimage-ga12ru12.rhcloud.com:8443');
